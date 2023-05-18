017c360ec3cd26fe5158dfaf9463c8417c24c992
Stephen Gambill <stephensgambill@gmail.com>
2023-05-18 15:03:59 -0500

    completed user list and nav linking

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -2,15 +2,18 @@ import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
 import { Authorized } from "./Authorized"
+import { UserList } from "../components/user/UserList"
+
 
 export const ApplicationViews = ({ token, setToken }) => {
   return <>
     <Routes>
-      <Route path="/login" element={<Login setToken={setToken} />}  />
-      <Route path="/register" element={<Register setToken={setToken} />}  />
+      <Route path="/login" element={<Login setToken={setToken} />} />
+      <Route path="/register" element={<Register setToken={setToken} />} />
       <Route element={<Authorized token={token} />}>
+        <Route path="/users" element={<UserList />} />
         {/* Add Routes here */}
-        
+
       </Route>
     </Routes>
   </>
