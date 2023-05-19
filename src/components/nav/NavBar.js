b0313459d77025a9bb28122835b3db18485615d8
Adam Olson <adajolson@yahoo.com>
2023-05-19 08:22:41 -0600

    Navbar changes

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -33,7 +33,7 @@ export const NavBar = ({ token, setToken }) => {
             token
               ?
               <div className="navbar-start">
-              <Link to="/" className="navbar-item">Posts</Link>
+              <Link to="/posts" className="navbar-item">Posts</Link>
               <Link to="tags" className="navbar-item">Tag Management</Link>
               </div>
               :
