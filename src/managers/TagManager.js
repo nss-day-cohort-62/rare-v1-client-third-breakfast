e5f79b4192c2797950a51a6d1ccada9ed2d5ae28
Annaliese Mathews <amesibov043@gmail.com>
2023-05-19 16:00:40 -0500

    added the ability to edit and delete tags. There is a pop up window before final delete.

--- src/managers/TagManager.js
+++ src/managers/TagManager.js
@@ -18,3 +18,33 @@ export const addTag = (tag) => {
     })
     .then(res => res.json())
 }
+
+export const getTag = (id) => {
+    return fetch(`http://localhost:8000/tags/${id}`, {
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("auth_token")}`
+        }
+    })
+        .then(res => res.json())
+}
+
+export const updateTag = (tag) => {
+    return fetch(`http://localhost:8000/tags/${tag.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Token ${localStorage.getItem("auth_token")}`
+        },
+        body: JSON.stringify(tag)
+    })
+}
+
+export const deleteTag = (id) => {
+    return fetch(`http://localhost:8000/tags/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Token ${localStorage.getItem("auth_token")}`
+        }
+    })
+}
